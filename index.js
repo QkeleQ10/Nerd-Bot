f22cd5550d4941754012ba3077b608cfc7c581d0
QkeleQ10 <quinten@althues.nl>
2024-06-18 00:12:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file);
     const event = require(filePath);
-    console.log(event)
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args));
     } else {
