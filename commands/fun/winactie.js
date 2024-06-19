f0af255ae7182fb05804ddd9fba0a322a0bbc22f
QkeleQ10 <quinten@althues.nl>
2024-06-19 14:01:53 +0200

    Update winactie.js

--- commands/fun/winactie.js
+++ commands/fun/winactie.js
@@ -50,6 +50,7 @@ module.exports = {
         await interaction.showModal(modal);
         await interaction.awaitModalSubmit({ time: 120_000, filter: modalInteraction => !!modalInteraction })
             .then(async modalInteraction => {
+                let explanation = "Je inzending is ongeldig. Kopieer je inzending vanuit Magister door `Ctrl` `Shift` `\\` in te drukken en te rechtsklikken op het Magister-logo. Probeer het vervolgens opnieuw."
                 try {
                     const verification = Number(modalInteraction.fields.getTextInputValue('verification'))
                     const expectedAnsw = operator === '+'
@@ -57,13 +58,14 @@ module.exports = {
                         : random1 - random2
 
                     if (isNaN(verification) || verification !== expectedAnsw) {
-                        throw new Error(`Je hebt de rekenvraag niet goed beantwoord (${random1} ${operator} ${random2} ≠ ${modalInteraction.fields.getTextInputValue('verification')}). Probeer het opnieuw.`)
+                        explanation = `Je hebt de rekenvraag niet goed beantwoord (${random1} ${operator} ${random2} ≠ ${modalInteraction.fields.getTextInputValue('verification')}). Probeer het opnieuw.`
+                        throw new Error()
                     }
 
                     const submission = { userId: modalInteraction.user.id, ...JSON.parse(atob(modalInteraction.fields.getTextInputValue('submission'))) }
 
                     if (!submission.title === 'Magister Theme Contest' || !submission.name?.length > 4 || !submission.school?.length > 4 || !submission.options) {
-                        throw new Error("Je inzending is ongeldig. Kopieer je inzending vanuit Magister door `Ctrl` `Shift` `\\` in te drukken en te rechtsklikken op het Magister-logo. Probeer het vervolgens opnieuw.")
+                        throw new Error()
                     }
 
                     const channelId = '1252718355291308072';
@@ -71,14 +73,15 @@ module.exports = {
                     const messages = await channel.messages.fetch();
 
                     if (messages.some((msg) => JSON.parse(msg.content.slice(7, -3))?.userId === modalInteraction.user.id || JSON.parse(msg.content.slice(7, -3))?.name === submission.name)) {
-                        throw new Error("Je hebt al eerder een inzending gedaan. Je mag maximaal één keer inzenden.")
+                        explanation = "Je hebt al eerder een inzending gedaan. Je mag maximaal één keer inzenden."
+                        throw new Error()
                     }
 
                     channel.send('```json\n' + JSON.stringify(submission, null, 2) + '```')
 
                     modalInteraction.reply({ content: "Thema ingezonden!", ephemeral: true })
                 } catch (error) {
-                    modalInteraction.reply({ content: `Thema niet ingezonden.\n\n${error}`.replace('Error: ', ''), ephemeral: true })
+                    modalInteraction.reply({ content: `Thema niet ingezonden.\n\n${explanation || error}`, ephemeral: true })
                 }
             })
             .catch(err => console.log('No modal submit interaction was collected'));
